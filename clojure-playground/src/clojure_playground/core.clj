06a4c3f99b672440b863270c1445775855dd202c
torsch <github@schalz.it>
2017-10-05 13:14:09 +0200

    hr - super reduced string - wip

--- clojure-playground/src/clojure_playground/core.clj
+++ clojure-playground/src/clojure_playground/core.clj
@@ -2,25 +2,47 @@
 
 (use '[clojure.string :only (split triml)])
 
-(def input-file  "/Users/tosch/hack4fun/clojure-playground/input.txt")
-(def output-file "/Users/tosch/hack4fun/clojure-playground/output.txt")
+(defn get-seq-length [s pos]
+  (if (or (< pos 0) (> (inc pos) (count s))) -1
+      (let [curpos (loop [curpos pos]
+                     (if (and (< (inc curpos) (count s)) (= (get s curpos) (get s (inc curpos))))
+                       (recur (inc curpos))
+                       curpos))]
+        (inc (- curpos pos)))))
 
-(defn solution []
+(defn is-seq? [s pos] (> (get-seq-length s pos) pos))
 
-  (let [T (Integer/parseInt (read-line))]
+(defn is-wrapped-seq? [s pos]
+  (let [seq-length (get-seq-length s pos)]
+    (if (or (zero? pos)
+            (<= (- (count s) (inc pos)) 1)
+            (< seq-length 2)
+            (> seq-length (count s)))
+      false
+      (and (not (= (get s (dec pos)) (get s pos)))
+           (= (get s (dec pos)) (get s (+ pos seq-length)))))))
 
-    (loop [a0 T]
-      (when (> a0 0)
 
-        (let [N   (Integer/parseInt (read-line))
-              arr (map #(Integer/parseInt %) (split (read-line) #"\s+"))]
+;; bool wSeqFound = false;
+;; do {
+;;     wSeqFound = false;
+;;     for( int i = 1; i < s.length() - 2; ++i ) {
+;;         if (isWrappedSeq(s, i)) {
+;;                                  s = s.mid(0, i) + s.mid(i+getSeqLength(s, i));
+;;                                  wSeqFound = true;
+;;                                  break;
+;;                                  }
+;;         }
+;;     } while (wSeqFound);
 
-          (println "Result="))
-
-        (recur (dec a0))))))
-
-(defn solve []
-  (with-in-str (slurp input-file) (solution)))
-
-(defn -main [& args]
-  (solve))
+;; bool seqFound = false;
+;; do {
+;;     seqFound = false;
+;;     for ( int i = 0; i < s.length() - 1; ++i ) {
+;;          if (getSeqLength(s, i)>1) {
+;;                                     s = s.mid(0, i) + s.mid(i+getSeqLength(s, i));
+;;                                     seqFound = true;
+;;                                     break;
+;;                                     }
+;;          }
+;;     } while (seqFound);
