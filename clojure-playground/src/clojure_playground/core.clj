afc6a6e5c227015739eaa4c50c5b51aadd6a447f
torsch <github@schalz.it>
2017-10-09 12:55:25 +0200

    performance...

--- clojure-playground/src/clojure_playground/core.clj
+++ clojure-playground/src/clojure_playground/core.clj
@@ -2,47 +2,29 @@
 
 (use '[clojure.string :only (split triml)])
 
-(defn get-seq-length [s pos]
-  (if (or (< pos 0) (> (inc pos) (count s))) -1
-      (let [curpos (loop [curpos pos]
-                     (if (and (< (inc curpos) (count s)) (= (get s curpos) (get s (inc curpos))))
-                       (recur (inc curpos))
-                       curpos))]
-        (inc (- curpos pos)))))
+(with-in-str (slurp "D:\\Cygwin\\home\\Developer\\hack4fun\\clojure-playground\\src\\clojure_playground\\input.txt")
 
-(defn is-seq? [s pos] (> (get-seq-length s pos) pos))
+  (let [n (Integer/parseInt (read-line))
+        s (into [] (into #{} (vec (map #(Integer/parseInt %) (split (read-line) #"\s+")))))
+        m (Integer/parseInt (read-line))
+        a (vec (map #(Integer/parseInt %) (split (read-line) #"\s+")))]
+    (loop [s s
+           a a]
+      (if (empty? a)
+        nil
+        (let [new-s (loop [new-s s] (if (> (first a) (last s)) new-s (recur (butlast s))))]
+          (println (inc (count new-s)))
+          (recur new-s (rest a)))))))
 
-(defn is-wrapped-seq? [s pos]
-  (let [seq-length (get-seq-length s pos)]
-    (if (or (zero? pos)
-            (<= (- (count s) (inc pos)) 1)
-            (< seq-length 2)
-            (> seq-length (count s)))
-      false
-      (and (not (= (get s (dec pos)) (get s pos)))
-           (= (get s (dec pos)) (get s (+ pos seq-length)))))))
-
-
-;; bool wSeqFound = false;
-;; do {
-;;     wSeqFound = false;
-;;     for( int i = 1; i < s.length() - 2; ++i ) {
-;;         if (isWrappedSeq(s, i)) {
-;;                                  s = s.mid(0, i) + s.mid(i+getSeqLength(s, i));
-;;                                  wSeqFound = true;
-;;                                  break;
-;;                                  }
-;;         }
-;;     } while (wSeqFound);
-
-;; bool seqFound = false;
-;; do {
-;;     seqFound = false;
-;;     for ( int i = 0; i < s.length() - 1; ++i ) {
-;;          if (getSeqLength(s, i)>1) {
-;;                                     s = s.mid(0, i) + s.mid(i+getSeqLength(s, i));
-;;                                     seqFound = true;
-;;                                     break;
-;;                                     }
-;;          }
-;;     } while (seqFound);
+;; (let [n (Integer/parseInt (read-line))
+;;       s (vec (map #(Integer/parseInt %) (split (read-line) #"\s+")))
+;;       m (Integer/parseInt (read-line))
+;;       a (vec (map #(Integer/parseInt %) (split (read-line) #"\s+")))]
+;;   (loop [s s
+;;          a a]
+;;     (if (empty? a)
+;;       nil
+;;       (let [rank 2]
+;;         (println (inc (count (filter #(> % (first a)) (into #{} s)))))
+;;         (recur s (rest a))))))
+;; )
